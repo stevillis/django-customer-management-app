1148bb7d5f5fbf23a27ac265cc4d831f03b27848
Stévillis Sousa <stevillis@hotmail.com>
2021-09-25 16:26:19 -0400

    Create templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,13 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('home')
+    return render(request, 'accounts/dashboard.html')
 
 
 def products(request):
-    return HttpResponse('products')
+    return render(request, 'accounts/customer.html')
 
 
 def customer(request):
-    return HttpResponse('customer')
+    return render(request, 'accounts/customer.html')
