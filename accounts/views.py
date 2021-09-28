b0a3cd3b5172f5138b6e3283c34075f629b8206d
Stévillis Sousa <stevillis@hotmail.com>
2021-09-28 02:21:24 -0400

    User Profile One to One Relationship

--- accounts/views.py
+++ accounts/views.py
@@ -126,6 +126,8 @@ def register_page(request):
             group = Group.objects.get(name='customer')
             user.groups.add(group)
 
+            Customer.objects.create(user=user, name=user.username, )
+
             messages.success(request, f'Account was created for {username}')
 
             return redirect('login')
@@ -157,6 +159,18 @@ def logout_user(request):
     return redirect('login')
 
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def user_page(request):
-    context = {}
+    orders = request.user.customer.order_set.all()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {
+        'orders': orders,
+        'total_orders': total_orders,
+        'delivered': delivered,
+        'pending': pending,
+    }
     return render(request, 'accounts/user.html', context)
